0ee0ae4d3a990f05f79a4f3de8d195c2cbfefef2
Aliaksandr Fedukovich <1632637@mail.ru>
2024-01-01 10:47:42 +0300

    refactor: add single style for selector according rule 3.1 in html-and-css.md

--- app.js
+++ app.js
@@ -8,7 +8,7 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("newTask");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
 var currentТasksHolder=document.getElementById("currentТaskss");//ul of #currentТaskss
 var completedTasksHolder=document.getElementById("completedTasks");//completedTasks
@@ -63,7 +63,7 @@ var createNewTaskElement=function(taskString){
 
 var addTask=function(){
     console.log("Add Task...");
-    //Create a new list item with the text from the #new-task:
+    //Create a new list item with the text from the #newTask:
     if (!taskInput.value) return;
     var listItem=createNewTaskElement(taskInput.value);
 
